f5d701a551cb8243471fe5f7d34d2f346b31c44f
pradeep.mascarenhas <paddum19@gmail.com>
2020-03-13 10:29:45 +0530

    First Commit

---
+++ E_Commerce/src/main/java/com/ownproj/invoice/container/ConnectionParam.java
@@ -0,0 +1,32 @@
+package com.ownproj.invoice.container;
+
+public class ConnectionParam {
+
+	public static final String DRIVER="com.mysql.cj.jdbc.Driver";  
+	public static final String URL="jdbc:mysql://localhost:3306/invoice_alpha?useSSL=false";
+	public static final String USERNAME="root";
+	public static final String PASSWORD="root";
+
+	//Pdf File Upload Path
+	public static final String PDF_UPLOAD_PATH="C:/Users/pradeep.mascarenhas/git/alphaInvoice2/openshifttester/tester/pdfFolder";
+	//public static final String PDF_UPLOAD_PATH="/opt/webserver/pdfFolder";
+	public static final String PDF_IMAGE_PATH="C:/Users/pradeep.mascarenhas/git/alphaInvoice2/openshifttester/tester/src/main/resources/ImageFolder";
+
+
+	/*public static final String DRIVER="com.mysql.cj.jdbc.Driver";  
+	public static final String URL="jdbc:mysql://mysql:3306/invoicedb?useSSL=false";
+	public static final String USERNAME="userF2K";
+	public static final String PASSWORD="KYkykFBldCBH05Tt";*/
+	
+	/*
+	 Openshift
+	  Username: userF2K
+	  Password: KYkykFBldCBH05Tt
+	  Database Name: invoicedb
+	 Connection URL: mysql://mysql:3306/
+
+	For more information about using this template, including OpenShift considerations, 
+	see https://github.com/sclorg/mysql-container/blob/master/5.7/root/usr/share/container-scripts/mysql/README.md .
+	 */
+	
+}
